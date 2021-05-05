a05dee3332da9e1620b1d1fd11415c4b0eddbd9e
Ling Xiang <blackpeppersama@gmail.com>
2021-05-04 20:03:22 -0700

    changes made

--- Test.java
+++ Test.java
@@ -2,4 +2,10 @@ public class Test {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
+
+	public static void helper() {
+		// A helper method
+		int a = 100;
+		System.out.println(a);
+	}
 }
---
+++ Test2.java
@@ -0,0 +1,6 @@
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("This is the second test file");
+	}
+
+}
